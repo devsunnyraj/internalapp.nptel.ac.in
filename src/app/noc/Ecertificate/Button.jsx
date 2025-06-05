df198da3b1696a1056affb0cb7f79bd0dc1bfa9e
devsunnyraj <hellotheresunnyhere@gmail.com>
2025-06-06 01:03:54 +0530

    initial changes

--- src/app/noc/Ecertificate/Button.jsx
+++ src/app/noc/Ecertificate/Button.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import '../../../styles/firstPage.css'
 
 const Button = () => {
+    const handleClick = () => {
+      const pdfUrl = `/NOC/NOC25/SEM1/Ecertificates/106/noc25-cs75/Course/NPTEL25CS75S24310149804466796.pdf`;
+      window.open(pdfUrl, '_blank', 'noopener,noreferrer');
+  };
     return (
         <>
-        <button id="firstButton">Course Certificate</button>
+        <button id="firstButton" onClick={handleClick}>Course Certificate</button>
         </>
     )
 }
